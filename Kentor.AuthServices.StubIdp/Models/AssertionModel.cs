39a9bf2cfa713ed2d26e3ece0f85915677d8de02
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2015-05-12 10:20:56 +0200

    Custom stubidp for the CGI/MVK IdP for citicens

--- Kentor.AuthServices.StubIdp/Models/AssertionModel.cs
+++ Kentor.AuthServices.StubIdp/Models/AssertionModel.cs
@@ -14,12 +14,25 @@ namespace Kentor.AuthServices.StubIdp.Models
 {
     public class AssertionModel
     {
+        private const string MVK_CGI_Subject_SerialNumber_ClaimType = "Subject_SerialNumber";
+        private const string MVK_CGI_Subject_GivenName_ClaimType = "Subject_GivenName";
+        private const string MVK_CGI_Subject_Surname_ClaimType = "Subject_Surname";
+
         [Required]
-        [Display(Name="Assertion Consumer Service Url")]
+        [Display(Name = "Assertion Consumer Service Url")]
         public string AssertionConsumerServiceUrl { get; set; }
 
         [Display(Name = "Subject NameId")]
         public string NameId { get; set; }
+
+        [Display(Name = "Subject SerialNumber (CivicNumber)")]
+        public string Subject_SerialNumber { get; set; }
+
+        [Display(Name = "Subject GivenName")]
+        public string Subject_GivenName { get; set; }
+
+        [Display(Name = "Subject Surname")]
+        public string Subject_Surname { get; set; }
 
         /// <summary>
         /// Creates a new Assertion model with values from web.config
@@ -39,16 +52,21 @@ namespace Kentor.AuthServices.StubIdp.Models
 
         public Saml2Response ToSaml2Response()
         {
-            var identity = new ClaimsIdentity(new Claim[] { 
-                new Claim(ClaimTypes.NameIdentifier, NameId )});
+            var identity = new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.NameIdentifier, NameId ),
+                new Claim(MVK_CGI_Subject_SerialNumber_ClaimType, Subject_SerialNumber),
+                new Claim(MVK_CGI_Subject_GivenName_ClaimType, Subject_GivenName),
+                new Claim(MVK_CGI_Subject_Surname_ClaimType, Subject_Surname),
+            }
+            );
 
             return new Saml2Response(
                 new EntityId(UrlResolver.MetadataUrl.ToString()),
-                CertificateHelper.SigningCertificate, new Uri(AssertionConsumerServiceUrl), 
+                CertificateHelper.SigningCertificate, new Uri(AssertionConsumerServiceUrl),
                 InResponseTo, identity);
         }
 
-        [Display(Name="Incoming AuthnRequest")]
+        [Display(Name = "Incoming AuthnRequest")]
         public string AuthnRequestXml { get; set; }
     }
 }
